a269bbdd133c8c4f5abbde393bb292d95683c296
Sam Mina <samandmissy2010@gmail.com>
2024-08-27 22:17:27 -0400

    Rewrote code for markdown formatting

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Read-Gen
-Professional README Generator
-I copied and pasted the code from the solved mini project to use as a guideline template for this challenge. 
+Professional README Generator. 
+I copied and pasted the code from the solved mini project to use as a guideline template for this challenge.
+I set up the gitignore and created my json files and downloaded my packages and dependencies. 
+I removed the HTML stuff and reformatted with the hashtags for the markdown headers and updated the questions. 
+
--- index.js
+++ index.js
@@ -1,68 +1,99 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 
-const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <header class="p-5 mb-4 header bg-light">
-    <div class="container">
-      <h1 class="display-4">Hi! My name is ${name}</h1>
-      <p class="lead">I am from ${location}.</p>
-      <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${github}</li>
-        <li class="list-group-item">LinkedIn: ${linkedin}</li>
-      </ul>
-    </div>
-  </header>
-</body>
-</html>`;
+const generateREADME = ({ title, description, installation, usage, contribution, tests, license, github, email }) =>
+  `# ${title}
+
+![License](https://img.shields.io/badge/license-${license}-blue.svg)
+
+## Description
+${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+This project is licensed under the ${license} license.
+
+## Contributing
+${contribution}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions, please contact me at [${email}](mailto:${email}).
+You can also find more of my work on my [GitHub profile](https://github.com/${github}).
+`;
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is the title of your project?',
     },
     {
       type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      name: 'description',
+      message: 'Provide a short description of your project:',
     },
     {
       type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      name: 'installation',
+      message: 'Provide the installation instructions:',
     },
     {
       type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username',
+      message: 'Enter your GitHub username:',
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      name: 'email',
+      message: 'Enter your email address:',
     },
   ])
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
-
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    const readmeContent = generateREADME(answers);
+    const filePath = path.join(process.cwd(), 'README.md');
+    fs.writeFile(filePath, readmeContent, (err) =>
+      err ? console.error(err) : console.log('Successfully created README.md!')
     );
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
   });
-
